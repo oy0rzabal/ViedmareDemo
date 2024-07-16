91b8de21e7d8701cb01b99475cfca873225b2a27
oy0rzabal <67671626+oy0rzabal@users.noreply.github.com>
2024-07-15 18:45:21 -0600

    Update mi.py

--- mi.py
+++ mi.py
@@ -1,15 +1,7 @@
 
-import matplotlib.pyplot as plt
 import streamlit as st
-import pandas as pd 
 from streamlit_extras.metric_cards import style_metric_cards
-import numpy as np
-from numerize.numerize import numerize
-import plotly.express as px
-import altair as alt
-import plotly.graph_objects as go
-import plotly.subplots as sp
-import altair as alt
+
 from db import execute_query
 #option menu
 import pandas as pd
